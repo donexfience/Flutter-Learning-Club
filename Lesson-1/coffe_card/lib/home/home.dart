6de791987ff6ffb862c4e63be7b4970e578d640a
donexfience <donex6fience@gmail.com>
2024-10-25 15:22:50 +0530

    stf && stless widgets buttons

--- Lesson-1/coffe_card/lib/home/home.dart
+++ Lesson-1/coffe_card/lib/home/home.dart
@@ -1,18 +1,43 @@
 
-
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
+  void increaseStrength(){
 
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:const Text('home page'),
-      backgroundColor: Colors.yellowAccent,
-      centerTitle: true,
+      appBar: AppBar(
+        title: const Text('home page'),
+        backgroundColor: Colors.yellowAccent,
+        centerTitle: true,
       ),
-      body:const Center(child: Text("hello this is home page")),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Expanded(
+                child: Container(
+              color: Colors.red,
+            )),
+            TextButton(onPressed: increaseStrength, child: const Text("click me")),
+          ],
+      
+        ),
+      ),
     );
   }
 }
+class NewHome extends StatefulWidget{
+  const NewHome({super.key});
+  @override
+  State<NewHome> createState() => _NewHomeState();
+}
+class _NewHomeState extends State<NewHome>{
+  @override
+  Widget build(BuildContext context) {
+    return const Text("hello");
+  }
+}
+
--- Lesson-1/coffe_card/lib/main.dart
+++ Lesson-1/coffe_card/lib/main.dart
@@ -1,6 +1,37 @@
 import 'package:coffe_card/home/home.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Home()));
+  runApp(const MaterialApp(title: 'coffe card', home: Home()));
 }
+
+// class Sandbox extends StatelessWidget {
+//   const Sandbox({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Column(
+//         children: [
+//           Column(
+//             children: [
+//               Expanded(
+//                 child: const Text(
+//                   "donex",
+//                   style: TextStyle(
+//                     fontFamily: "roboto",
+//                     fontSize: 20,
+//                     color: Colors.blue,
+//                   ),
+//                 ),
+//               ),
+//               Image.asset('assets/images/coffe.png'),
+//             ],
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
